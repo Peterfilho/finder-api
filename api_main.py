8a7b05f7d93347894d7f423bca314909ac702d38
Peterson <peterson.petio@gmail.com>
2020-05-09 23:54:55 -0300

    Fix spaces in search

--- api_main.py
+++ api_main.py
@@ -235,10 +235,14 @@ def positions_post_app():
     date = data['date']
 #    print("valor que chega!")
 #    print(find)
+    valores = valores.lstrip(' ')
+    print("valores sem espaço")
+    print(valores)
+    
     valores = find.split(",")
     print("lista de inteiros: valores")
     print(valores)
-#        aux = localize.localize(-17,-47 ,-100,-55,-100,-47)
+#        aux = localize.localize(-17,-47,-100,-55,-100,-47)
 #        aux = localize.localize(-100,-70,-68,-55,-53,-55)
 #        valores[0],valores[1],valores[2],valores[3],valores[4],valores[5]
 
